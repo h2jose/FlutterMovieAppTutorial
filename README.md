9e1e70cfd7129b93635e126fff2d6fcc2eaad7b6
Jose Hernandez <h2jose@gmail.com>
2018-10-20 11:01:54 -0400

    Tutorial No3
    Signed-off-by: Jose Hernandez <h2jose@gmail.com>

--- README.md
+++ README.md
@@ -5,9 +5,16 @@ A new Flutter application demo by [Daniel Ortiz @daniorca ](https://github.com/d
 ## Getting Started
 
 ### STEPS (see Branchs):
-- Tutorial 1: Introduction and installation (It is not here, read docs)
-- Tutorial 2: API Json Configuration
+- **Tutorial 1**: Introduction and installation (Not here, read docs).
 
+
+- **Tutorial 2**: API Json Configuration
+
+  Files:
+  + main.dart
+  + models/modieModel.dart
+
+----
 
 For help getting started with Flutter, view our online
 - [EN documentation](https://flutter.io/).
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import './models/movieModel.dart';
+import './models/tmdb.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 const baseUrl = "https://api.themoviedb.org/3/movie/";
 const baseImageUrl = "https://image.tmdb.org/t/p/";
@@ -10,11 +12,11 @@ const apiKey = "03400e684b0d8c24d655190914d4aab8";
 const nowPlayingUrl = "${baseUrl}now_playing?api_key=$apiKey";
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  title: 'Movie App',
-  theme: ThemeData.dark(),
-  home: MyMovieApp(),
-));
+      debugShowCheckedModeBanner: false,
+      title: 'Movie App',
+      theme: ThemeData.dark(),
+      home: MyMovieApp(),
+    ));
 
 class MyMovieApp extends StatefulWidget {
   @override
@@ -39,6 +41,16 @@ class _MyMovieApp extends State<MyMovieApp> {
     });
   }
 
+  Widget _buildCarouselSlider() => CarouselSlider(
+        items: nowPlayingMovies.results
+            .map((movieItem) =>
+                Image.network("${baseImageUrl}w342${movieItem.posterPath}"))
+            .toList(),
+        autoPlay: false,
+        height: 240.0,
+        viewportFraction: 0.5,
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,6 +69,55 @@ class _MyMovieApp extends State<MyMovieApp> {
             onPressed: () {},
           )
         ],
+      ),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBookIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              title: Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text('NOW PLAYING',
+                      style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ),
+              expandedHeight: 290.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Stack(
+                  children: <Widget>[
+                    Container(
+                      child: Image.network(
+                        "${baseImageUrl}w500/2uNW4WbgBXL25BAbXGLnLqX71Sw.jpg",
+                        fit: BoxFit.cover,
+                        width: 1000.0,
+                        colorBlendMode: BlendMode.dstATop,
+                        color: Colors.blue.withOpacity(0.5),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 35.0),
+                      child: Column(
+                        children: nowPlayingMovies == null
+                            ? <Widget>[
+                                Center(
+                                  child: CircularProgressIndicator(),
+                                )
+                              ]
+                            : <Widget>[_buildCarouselSlider()],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ];
+        },
+        body: Center(child: Text('Scroll me')),
       ),
     );
   }
